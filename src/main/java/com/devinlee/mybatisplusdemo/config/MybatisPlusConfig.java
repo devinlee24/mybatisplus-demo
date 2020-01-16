117834105fd9a6d79865c05bfacc076543aa731d
devinlee <devinlee24@foxmail.com>
2020-01-16 18:44:44 +0800

    SQL执行效率插件

--- src/main/java/com/devinlee/mybatisplusdemo/config/MybatisPlusConfig.java
+++ src/main/java/com/devinlee/mybatisplusdemo/config/MybatisPlusConfig.java
@@ -1,9 +1,11 @@
 package com.devinlee.mybatisplusdemo.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class MybatisPlusConfig {
@@ -22,5 +24,14 @@ public class MybatisPlusConfig {
         // 开启 count 的 join 优化,只针对部分 left join
         paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
         return paginationInterceptor;
+    }
+
+    /**
+     * SQL执行效率插件
+     */
+    @Bean
+    //@Profile({"dev","test"})// 设置 dev test 环境开启
+    public PerformanceInterceptor performanceInterceptor() {
+        return new PerformanceInterceptor();
     }
 }
